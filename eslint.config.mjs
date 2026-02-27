9f65a56c3a2406b34cae1793945112c277e87c9f
Justin Hernandez <justin.hernandez@self.xyz>
2026-02-26 23:50:59 -0800

    better rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -107,6 +107,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/require-await": "off",
       "@typescript-eslint/no-floating-promises": "off",
       "@typescript-eslint/await-thenable": "off",
